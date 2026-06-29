0e2ee066fed612e4051c52bddd2209540e84bc22
adampierce10-oss <adampierce10@gmail.com>
2026-06-28 18:42:22 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const avatars = [
+  "Lightning Hero",
+  "Fire Hero",
+  "Ice Hero",
+  "Robot Hero",
+  "Galaxy Hero",
+  "Ninja Hero",
+  "Speed Hero",
+  "Shadow Hero"
+];
+
+const difficulties = {
+  1: { name: "Sidekick", multiplier: 1 },
+  1.5: { name: "Hero", multiplier: 1.5 },
+  2: { name: "Superhero", multiplier: 2 },
+  3: { name: "Legend", multiplier: 3 }
+};
